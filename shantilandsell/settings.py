03f1423f3ad4efe9a5499f9e4f4148129da3f81e
RASHEL <rashel47mia47@gmail.com>
2023-11-23 15:00:00 -0800

    2nd Commit

--- shantilandsell/settings.py
+++ shantilandsell/settings.py
@@ -169,6 +169,7 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user-uploaded files)
 MEDIA_URL = '/media/'
